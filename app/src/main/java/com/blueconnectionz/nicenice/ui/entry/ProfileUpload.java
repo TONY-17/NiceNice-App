4762a09e602df9aebd50890b14c10f8ae6a67bff
TonyLebea <Lebeatony.tl@gmail.com>
2022-11-16 14:46:12 +0200

    Implemented image pickers

--- app/src/main/java/com/blueconnectionz/nicenice/ui/entry/ProfileUpload.java
+++ app/src/main/java/com/blueconnectionz/nicenice/ui/entry/ProfileUpload.java
@@ -1,24 +1,29 @@
 package com.blueconnectionz.nicenice.ui.entry;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.Window;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.OvershootInterpolator;
 
 import com.blueconnectionz.nicenice.R;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 public class ProfileUpload extends AppCompatActivity {
+
+    int SELECT_PICTURE = 200;
+    int REQUEST_IMAGE_CAPTURE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +45,44 @@ public class ProfileUpload extends AppCompatActivity {
         animation.setInterpolator(new DecelerateInterpolator());
         animation.start();
 
-        MaterialButton upload = findViewById(R.id.uploadPicture);
-        upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ProfileUpload.this,DocumentUpload.class));
-            }
+        MaterialButton openCamera = findViewById(R.id.openCamera);
+        openCamera.setOnClickListener(view -> {
+            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(camera_intent, REQUEST_IMAGE_CAPTURE);
         });
+
+        // Enables a driver to select an image from gallery
+        MaterialButton selectPictureFromGallery = findViewById(R.id.openGallery);
+        selectPictureFromGallery.setOnClickListener(view -> openGalleryFolder());
+    }
+
+
+    private void  openGalleryFolder(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            // Select image from camera
+
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                //imageView.setImageBitmap(imageBitmap);
+            }
+            // Select image from gallery
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    //IVPreviewImage.setImageURI(selectedImageUri);
+                }
+            }
+        }
+
     }
 }
